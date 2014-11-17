c9598ff361a7b65596612df9114ed066b82cb731
fpancald <fpancald@nd.edu>
2014-11-17 17:47:54 -0500

    add testevo and changes

---
+++ Homeworks/testevo.m
@@ -0,0 +1,51 @@
+told=testfitness(100,v);
+n=length(v);
+for j=1:n
+    iter=10;
+    cng=0.1;
+for i=1:iter
+v(j,1)=v(j,1)+cng;
+tnew=testfitness(100,v);
+if told>=tnew
+    v(j,1)=v(j,1)-cng;
+    break;
+else
+    told=tnew
+end
+end
+
+for i=1:iter
+v(j,1)=v(j,1)-cng;
+tnew=testfitness(100,v);
+if told>=tnew
+    v(j,1)=v(j,1)+cng;
+    break;
+else
+    told=tnew
+end
+end
+
+for i=1:iter
+v(j,2)=v(j,2)+cng;
+tnew=testfitness(100,v);
+if told>=tnew
+    v(j,2)=v(j,2)-cng;
+    break;
+else
+    told=tnew
+end
+end
+
+for i=1:iter
+v(j,2)=v(j,2)-cng;
+tnew=testfitness(100,v);
+if told>=tnew
+    v(j,2)=v(j,2)+cng;
+    break;
+else
+    told=tnew
+end
+end
+
+end
+told
